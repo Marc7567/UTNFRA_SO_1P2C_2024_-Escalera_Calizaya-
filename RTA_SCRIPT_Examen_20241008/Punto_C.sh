f3d7576efcefdc9b6292bb18f4ec29a64e847fd2
root <root@VMPruebas2>
2024-10-11 17:42:23 +0000

    9no y posible ultimo commit

--- RTA_SCRIPT_Examen_20241008/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_C.sh
@@ -4,25 +4,25 @@ sudo groupadd p1c2_2024_gAlumno
 sudo groupadd p1c2_2024_gProfesores
 echo "creamos los usuarios y sus contrasenas, luego los agregamos a sus respectivos grupos"
 for i in 1 2 3 4; do
- if [$i -eq 4]; then
-  sudo useradd -m -s /bin/bash -c "profesor 1" -G p1c2_2024_gProfesores -p "$(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
- else
-  sudo useradd -m -s /bin/bash -c "Alumno $i" -G p1c2_2024_gAlumno -p "$(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A$i
- fi
+    if [$i -eq 4]; then
+        sudo useradd -m -s /bin/bash -c "profesor 1" -G p1c2_2024_gProfesores -p "$(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
+    else
+        sudo useradd -m -s /bin/bash -c "Alumno $i" -G p1c2_2024_gAlumno -p "$(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_A$i
+    fi
 echo "ahora cambiamos el grupo y propietario a las carpetas alumnos y profesores"
 for i in 1 2 3 4; do
- if [$i -eq 4]; then
-  sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores
- else
-  sudo chown p1c2_2024_A$i:p1c2_2024_A$i /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_$i
- fi
+    if [$i -eq 4]; then
+        sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores
+    else
+        sudo chown p1c2_2024_A$i:p1c2_2024_A$i /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_$i
+    fi
 echo "crear archivo validar.txt con whoami"
 for i in 1 2 3 4; do
- if [$i -eq 4]; then
-  whoami > validar.txt | mv validar.txt /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores
- else
-  whoami > validar.txt | mv validar.txt /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_$i
- fi
+    if [$i -eq 4]; then
+        whoami > validar.txt | mv validar.txt /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores
+    else
+        whoami > validar.txt | mv validar.txt /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_$i
+    fi
 echo "ahora cambiamos los permisos de las carpetas"
 sudo chmod 750 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_1
 sudo chmod 760 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/alumnos_2
@@ -30,8 +30,8 @@ sudo chmod 700 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Exa
 sudo chmod 775 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores
 echo "llamamos a los archivos validar"
 for i in 1 2 3 4; do
- if [$i -eq 4]; then
-  sudo -su -c "whoami > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
- else
-  sudo -su -c "whoami > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-//Examenes-UTN/alumnos_$i/validar.txt" p1c2_2024_A$i
- fi
+    if [$i -eq 4]; then
+        sudo -su -c "whoami > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
+    else
+        sudo -su -c "whoami > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-//Examenes-UTN/alumnos_$i/validar.txt" p1c2_2024_A$i
+    fi
--- RTA_SCRIPT_Examen_20241008/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_E.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 cd ..
 echo "creamos el archivo Filtro_Basico.txt"
-touch RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+touch /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
 echo "agregamos la info de Memoria Ram al archivo Filtro_Basico.txt"
-sudo cat /proc/meminfo | grep -i memtotal > RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+sudo cat /proc/meminfo | grep -i memtotal > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
 echo "agregamos la info del chassis del pc"
-sudo dmidecode -t chassis | grep -i cha >> RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
-sudo dmidecode -t chassis | grep -i manu >> RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep -i cha >> /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+sudo dmidecode -t chassis | grep -i manu >> /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
 echo "mostramos la info del archivo"
-cat RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
+echo ""
+cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/RTA_ARCHIVOS_Examen_20241008/Filtro_Basico.txt
