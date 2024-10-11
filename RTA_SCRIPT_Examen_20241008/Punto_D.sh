1e0417b0aff4d0b8fd713c44c7ad4191fc00aabf
root <root@VMPruebas2>
2024-10-11 14:12:23 +0000

    modificado

--- RTA_SCRIPT_Examen_20241008/Punto_D.sh
+++ RTA_SCRIPT_Examen_20241008/Punto_D.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p Estructura_Asimetrica/{correo/cartas_{1..100},clientes/{cartas_{1..100},carteros_{1..10}}}
+mkdir -p /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_-Escalera_Calizaya-/Estructura_Asimetrica/{correo/cartas_{1..100},clientes/{cartas_{1..100},carteros_{1..10}}}
 tree Estructura_Asimetrica/ --noreport | pr -T -s' ' -w 80 --column 4
